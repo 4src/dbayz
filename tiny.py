5c42fec4b04d722aea8ef6f1b47d206a933853bc
Tim Menzies <timm@users.noreply.github.com>
2023-05-31 17:58:22 -0400

    saving

--- tiny.py
+++ tiny.py
@@ -1,28 +1,30 @@
 # vim: set et sts=2 sw=2 ts=2 :
 """
-tiny: look around just a little, then find some good stuff
-(c) 2023, Tim Menzies, <timm@ieee.org>  BSD-2
+# SYNOPSIS:
+  tiny: look around just a little, then guess where is the good stuff
+  (c) 2023, Tim Menzies, <timm@ieee.org>  BSD-2
 
-USAGE: ./tiny.py [OPTIONS] [-g ACTIONS]
+## USAGE:
+  ./tiny.py [OPTIONS] [-g ACTIONS]
 
-DESCRIPTION:
+## DESCRIPTION:
   Use to find best regions within rows of data with multiple objectives.
   N rows of data are ranked via a multi-objective domination predicate
   and then discretized, favoring ranges that distinguish the best
   (N^min) items from a sample of the rest*(N^min)
 
-OPTIONS:
+## OPTIONS:
 
      -b  --bins    max number of bins    = 16  
      -c  --cohen   size significant separation = .35  
      -f  --file    data csv file         = ../data/auto93.csv  
      -g  --go      start up action       = nothing  
      -h  --help    show help             = False  
-     -k  --keep    how many nums to keep = 512
-     -m  --min     min size              = .5
-     -r  --rest    ratio best:rest       = 3
+     -k  --keep    how many nums to keep = 512  
+     -m  --min     min size              = .5  
+     -r  --rest    ratio best:rest       = 3  
      -s  --seed    random number seed    = 1234567891  
-     -w  --want    what goal to chase    = mitigate
+     -w  --want    what goal to chase    = mitigate  
 """
 import random,math,sys,ast,re
 from termcolor import colored
@@ -30,15 +32,19 @@ from functools import cmp_to_key
 
 the={} # global options, filled in later
 
-def isa(x,y): return isinstance(x,obj) and x.isa == y
+class obj(object):
+  oid = 0
+  def __init__(i,**kw): obj.oid+=1; i.__dict__.update(_id=obj.oid, **kw)
+  def __repr__(i)     : return printd(i.__dict__)
+  def __hash__(i)     : return i._id
 
 def SYM(at=0,txt=""):
-  return obj(isa=SYM,txt=txt, at=at, n=0,
+  return obj(this=SYM,txt=txt, at=at, n=0,
              counts={}, mode=None, most=0)
 
 def NUM(at=0,txt=""):
    w = -1 if txt and txt[-1]=="-" else 1
-   return obj(isa=NUM,txt=txt, at=at, n=0,
+   return obj(this=NUM,txt=txt, at=at, n=0,
               _kept=[], ok=True, w=w, lo=inf, hi=-inf)
 
 def norm(num,x):
@@ -47,34 +53,34 @@ def norm(num,x):
 def add(col,x,n=1):
   if x == "?": return
   col.n += n
-  if isa(col,NUM):
+  if col.this is SYM:
+    now = col.counts[x] = 1 + col.counts.get(x,0)
+    if now > col.most: col.most, col.mode = now, x
+  else:
     col.lo = min(x, col.lo)
     col.hi = max(x, col.hi)
     a = col._kept
     if   len(a) < the.keep    : col.ok=False; a += [x]
     elif r() < the.keep/col.n : col.ok=False; a[int(len(a)*r())] = x
-  else:
-    now = col.counts[x] = 1 + col.counts.get(x,0)
-    if now > col.most: col.most, col.mode = now, x
 
 def ok(col):
-  if isa(col,NUM) and not col.ok:
+  if col.this is NUM and not col.ok:
     col._kept.sort()
     col.ok=True
   return col
 
 def div(col,decimals=None):
-  return rnd(stdev(ok(col)._kept) if isa(col,NUM) else ent(col.counts), decimals)
+  return rnd(ent(col.counts) if col.this is SYM else stdev(ok(col)._kept),decimals)
 
 def mid(col,decimals=None):
-  return rnd(median(ok(col)._kept),decimals) if isa(col,NUM) else col.mode
+  return col.mode if col.this is SYM else rnd(median(ok(col)._kept),decimals)
 
 #---------------------------------------------------------------------------------------------------
 def ROW(cells=[]):
-  return obj(isa=ROW,cells=cells)
+  return obj(this=ROW,cells=cells)
 
 def COLS(names):
-  cols = obj(isa=COLS,names=None, x=[], y=[], all=[])
+  cols = obj(this=COLS,names=None, x=[], y=[], all=[])
   cols.names = names
   for n,s in enumerate(names):
     col = (NUM if s[0].isupper() else SYM)(at=n,txt=s)
@@ -83,17 +89,16 @@ def COLS(names):
       (cols.y if s[-1] in "-+!" else cols.x).append(col)
   return cols
 
-def DATA(data=None, src=[], filter=lambda x:x):
-  data = data or obj(isa=DATA,rows=[], cols=None)
+def DATA(data=None, src=[]):
+  data = data or obj(this=DATA,rows=[], cols=None)
   for row in src:
     if not data.cols:
       data.cols = COLS(row)
     else:
-      lst = row.cells if isa(row,ROW) else row
+      row = ROW(row) if isinstance(row,list) else row
       for cols in [data.cols.x, data.cols.y]:
         for col in cols:
-          x = lst[col.at] = filter(lst[col.at])
-          add(col,x)
+          add(col,row.cells[col.at])
       data.rows += [row]
   return data
 
@@ -112,7 +117,7 @@ def better(data, row1, row2):
   "`Row1` is better than `row2` if moving to it losses less than otherwise."
   s1, s2, cols, n = 0, 0, data.cols.y, len(data.cols.y)
   for col in cols:
-    a, b = norm(col,row1[col.at]), norm(col,row2[col.at])
+    a, b = norm(col,row1.cells[col.at]), norm(col,row2.cells[col.at])
     s1  -= math.exp(col.w * (a - b) / n)
     s2  -= math.exp(col.w * (b - a) / n)
   return s1 / n < s2 / n
@@ -120,18 +125,23 @@ def better(data, row1, row2):
 def ordered(data,rows=[]):
   return sorted(rows or data.rows,
                 key=cmp_to_key(lambda r1,r2: better(data,r1,r2)))
-
 #---------------------------------------------------------------------------------------------------
 def discretize(col,x):
   if x == "?": return
-  if isa(col,NUM):
+  if col.this is NUM:
     x = int(the.bins*(x - col.lo)/(col.hi - col.lo + 1/inf))
     x = min(the.bins, max(0, x))
   return x
 
-def BIN(at=0,txt=" ",lo=None,hi=None,B=0,R=0):
-  return obj(at=at, txt=txt, lo=lo or inf, hi=hi or lo,
-             n=0, ys={}, score=0, B=B, R=R)
+class BIN(object):
+  def __init__(self,at=0,txt=" ",lo=None,hi=None,B=0,R=0):
+    self.at=at; self.txt=txt; self.lo=lo or inf; self.hi=hi or lo
+    self.n=0; self.ys={}; self.score=0; self.B=B; self.R=R
+  def __repr__(self):
+    if self.hi == self.lo: return f"{self.txt}={self.lo}"
+    if self.hi == inf:     return f"{self.txt}>={self.lo}"
+    if self.lo == -inf:    return f"{self.txt}<{self.hi}"
+    return f"{self.lo} <= {self.txt} < self.hi"
 
 def binAdd(bin, x, y, row):
   bin.n     += 1
@@ -147,9 +157,9 @@ def merge(bin1, bin2):
             B=bin1.B, R=bin2.R)
   out.n = bin1.n + bin2.n
   for d in [bin1.ys, bin2.ys]:
-    for key in d:
-      old = out.ys[key]  = out.ys.get(key,set())
-      out.ys[key]  = old | d[key]
+    for klass in d:
+      old = out.ys[klass]  = out.ys.get(klass,set())
+      out.ys[klass]  = old | d[klass]
   return out
 
 def merged(bin1,bin2,num,best):
@@ -163,24 +173,34 @@ def merged(bin1,bin2,num,best):
   if e3 <= (bin1.n*e1 + bin2.n*e2)/out.n : return out
 
 def binEnt(bin):
-  return ent({k:len(lst) for k,lst in bin.ys.items()})
+  return ent({k:len(set) for k,set in bin.ys.items()})
 
-def contrasts(data1,data2):
+def contrasts(data1,data2, elite=False):
+  if elite:
+    enough = None
+    for bin in sorted(contrasts1(data1,data2),reverse=True,key=lambda bin: bin.score):
+      enough = enough or bin.score *.1
+      if not(bin.lo == -inf and bin.hi == inf) and bin.score > enough:
+        yield bin
+  else:
+    for bin in contrasts1(data1,data2): yield bin
+
+def contrasts1(data1,data2):
   data12 = clone(data1, data1.rows + data2.rows)
   for col in data12.cols.x:
     bins = {}
     for klass,rows in dict(best=data1.rows, rest=data2.rows).items():
       for row in rows:
-        x = row[col.at]
+        x = row.cells[col.at]
         if z := discretize(col, x):
           if z not in bins: bins[z] = BIN(at=col.at,txt=col.txt,lo=x,
                                           B=len(data1.rows), R=len(data2.rows))
           binAdd(bins[z], x, klass, row)
     for bin in merges(col, sorted(bins.values(), key=lambda z:z.lo),"best"):
-      yield value(bin, col)
+        yield value(bin, col)
 
 def merges(col,bins,best):
-  if not isa(col,NUM): return bins
+  if col.this is SYM: return bins
   bins = mergeds(bins,col,best)
   for j in range(len(bins)-1): bins[j].hi = bins[j+1].lo
   bins[0].lo = -inf
@@ -198,7 +218,7 @@ def mergeds(a, col,best):
   return a if len(a) == len(b) else mergeds(b, col,best)
 
 def value(bin,col):
-  b,r = bin.ys.get("best",[]), bin.ys.get("rest",[])
+  b,r = bin.ys.get("best",set()), bin.ys.get("rest",set())
   bin.score = want(len(b),len(r), bin.B, bin.R)
   return bin
 
@@ -212,21 +232,22 @@ def want(b,r,B,R):
     case "xplore":   return (b+r)/abs(b - r)
 
 #---------------------------------------------------------------------------------------------------
+r=random.random
 inf=float("inf")
+
 class obj(object):
   oid = 0
   def __init__(i,**kw): obj.oid+=1; i.__dict__.update(_id=obj.oid, **kw)
   def __repr__(i)     : return printd(i.__dict__)
   def __hash__(i)     : return i._id
 
-r=random.random
 def red(s): return colored(s,"red",attrs=["bold"])
 def green(s): return colored(s,"green",attrs=["bold"])
 def yellow(s): return colored(s,"yellow",attrs=["bold"])
 def bold(s): return colored(s,"white",attrs=["bold"])
 
 def rnd(x,decimals=None):
-   return round(x,decimals) if decimals else  x
+  return round(x,decimals) if decimals else  x
 
 def per(a,p=.5):
   n = max(0, min(len(a)-1, int( 0.5 + p*len(a))))
@@ -238,6 +259,11 @@ def ent(a):
   N = sum((a[k] for k in a))
   return - sum(a[k]/N * math.log(a[k]/N,2) for k in a if a[k] > 0)
 
+def powerset(s):
+  r = [[]]
+  for e in s: r += [x+[e] for x in r]
+  return r[1:]
+
 def coerce(x):
   try: return ast.literal_eval(x)
   except: return x
@@ -245,15 +271,6 @@ def coerce(x):
 def printd(d):
   p= lambda x: '()' if callable(x) else (f"{x:.2f}" if isinstance(x,float) else str(x))
   return "{"+(" ".join([f":{k} {p(v)}" for k,v in d.items() if k[0]!="_"]))+"}"
-
-def cli(d):
-  d1=d.__dict__
-  for k,v in d1.items():
-    v = str(v)
-    for i,x in enumerate(sys.argv):
-      if ("-"+k[0]) == x or ("--"+k) == x:
-        d1[k]= coerce("True" if v=="False" else ("False" if v=="True" else sys.argv[i+1]))
-  return d
 
 def csv(file):
   with open(file) as fp:
@@ -263,8 +280,19 @@ def csv(file):
         yield [coerce(s.strip()) for s in line.split(",")]
 
 def settings(s):
-  return obj(**{m[1]:coerce(m[2])
-                for m in re.finditer(r"\n\s*-\w+\s*--(\w+)[^=]*=\s*(\S+)",s)})
+  setting = r"\n\s*-\w+\s*--(\w+)[^=]*=\s*(\S+)"
+  return obj(**{m[1]:coerce(m[2]) for m in re.finditer(setting,s)})
+#---------------------------------------------------------------------------------------------------
+def runs():
+  if the.help:
+    headings, flags ="\n[#]+ [A-Z][A-Z]+:"," [-][-]?[\S]+"
+    show  = lambda f: lambda m: f(m[0])
+    print(re.sub(headings, show(yellow),re.sub(flags, show(bold), __doc__)))
+  else:
+    n= sum([run(s,fun) for s,fun in egs.items()
+                       if s[0]!="_" and the.go in ["all",s]])
+    print(red(f"{n} FAILURE(S)") if n>0 else green(f"{n} FAILURE(S)"))
+    return n
 
 def run(s,fun):
   d = the.__dict__
@@ -276,22 +304,23 @@ def run(s,fun):
   print(red("FAIL") if out==False else green("PASS"))
   return out==False
 
-def runs():
-  if the.help:
-    print(re.sub("\n[A-Z][A-Z]+:", lambda m:yellow(m[0]), 
-                 re.sub(" [-][-]?[\S]+", lambda m:bold(m[0]), __doc__)))
-  else:
-    n= sum([run(s,fun) for s,fun in egs.items()
-                       if s[0]!="_" and the.go in ["all",s]])
-    print(red(f"{n} FAILURES") if n>0  else green(f"{n} FAILURES"))
-    return n
-
+def cli(d):
+  d1=d.__dict__
+  for k,v in d1.items():
+    v = str(v)
+    for i,x in enumerate(sys.argv):
+      if ("-"+k[0]) == x or ("--"+k) == x:
+        d1[k]= coerce("True" if v=="False" else ("False" if v=="True" else sys.argv[i+1]))
+  return d
 #---------------------------------------------------------------------------------------------------
 egs={}
 def eg(f): egs[f.__name__]= f; return f
 
 @eg
-def thed(): print(str(the)[:50],"... ",end=""); return False
+def thed(): print(str(the),"...",end=" ")
+
+@eg
+def powered(): print(powerset("abc"),end=" ")
 
 @eg
 def colnum():
@@ -336,24 +365,36 @@ def orderer():
   print("best", stats(clone(data,rows[-30:])))
   print("rest", stats(clone(data,rows[:30])))
 
-#@eg
-def contraster():
+@eg
+def contrasted():
   data = DATA(src=csv(the.file))
   rows = ordered(data)
   b = int(len(data.rows)**the.min)
-  r = b*the.rest
   best = clone(data,rows[-b:])
   #rest = clone(data,random.sample(rows[:-b],r))
-  rest = clone(data,rows[:r])
+  rest = clone(data,rows[:b*the.rest])
   print("\nall ", stats(data))
   print("best", stats(best))
   print("rest", stats(rest))
   b4 = None
   for bin in contrasts(best,rest):
     if bin.txt != b4: print("")
-    print(bin.txt, bin.lo, bin.hi,
-          {k:len(bin.ys[k]) for k in sorted(bin.ys)}, f"{bin.score:.2f}")
+    print(f"{bin.txt:10} {bin.lo:5} {bin.hi:5}",
+          obj(best=len(bin.ys.get("best",set())),
+              rest=len(bin.ys.get("rest",set())),
+              score=f"{bin.score:.2f}"))
     b4 = bin.txt
+
+@eg
+def ruled():
+  data = DATA(src=csv(the.file))
+  rows = ordered(data)
+  b = int(len(data.rows)**the.min)
+  best = clone(data,rows[-b:])
+  rest = clone(data,rows[:b*the.rest])
+  print("")
+  for bin in powerset(contrasts(best,rest, elite=True)):
+    print(bin)
 
 #---------------------------------------------------------------------------------------------------
 the = settings(__doc__)
